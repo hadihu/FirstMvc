518ba0741d3a3ae41e8306a20ca630043653c7cd
hadihu <869646722@qq.com>
2017-03-23 15:13:09 +0800

    03.23

--- FirstMvc/Models/EmployeeBusinessLayer.cs
+++ FirstMvc/Models/EmployeeBusinessLayer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using FirstMvc.Data_Access_Layer;
 
 namespace FirstMvc.Models
 {
@@ -9,26 +10,37 @@ namespace FirstMvc.Models
     {
         public List<Employee> GetEmployees()
         {
-            List<Employee> employees = new List<Employee>();
-            Employee emp = new Employee();
-            emp.FirstName = "johnson";
-            emp.LastName = " fernandes";
-            emp.Salary = 14000;
-            employees.Add(emp);
+            //List<Employee> employees = new List<Employee>();
+            //Employee emp = new Employee();
+            //emp.FirstName = "johnson";
+            //emp.LastName = " fernandes";
+            //emp.Salary = 14000;
+            //employees.Add(emp);
      
-            emp = new Employee();
-            emp.FirstName = "michael";
-            emp.LastName = "jackson";
-            emp.Salary = 16000;
-            employees.Add(emp);
+            //emp = new Employee();
+            //emp.FirstName = "michael";
+            //emp.LastName = "jackson";
+            //emp.Salary = 16000;
+            //employees.Add(emp);
      
-            emp = new Employee();
-            emp.FirstName = "robert";
-            emp.LastName = " pattinson";
-            emp.Salary = 20000;
-            employees.Add(emp);
+            //emp = new Employee();
+            //emp.FirstName = "robert";
+            //emp.LastName = " pattinson";
+            //emp.Salary = 20000;
+            //employees.Add(emp);
      
-            return employees;
+            //return employees;
+            SalesERPDAL saleDal = new SalesERPDAL();
+            return saleDal.Employees.ToList();
+        }
+
+        public Employee SaveEmployee(Employee e)
+        {
+            SalesERPDAL salesDal = new SalesERPDAL();
+            salesDal.Employees.Add(e);
+            salesDal.SaveChanges();
+            return e;
+
         }
     }
 
